ea4bdfbb9ed1126443faa87b92888ad75741fa7a
Christian Montero Martínez <christianmontero08@gmail.com>
2018-09-06 15:44:11 -0700

    Displaying the transactions in the home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, MenuController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, MenuController } from 'ionic-angular';
 
 import { AddtransactionPage } from '../addtransaction/addtransaction';
+
+//Firebase
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'page-home',
@@ -10,9 +14,22 @@ import { AddtransactionPage } from '../addtransaction/addtransaction';
 export class HomePage {
 
   username: string;
+  transaction_list;
+  student;
+  student_id;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private menu:MenuController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private db: AngularFireDatabase, private auth: AngularFireAuth, private menu: MenuController) {
     this.username = navParams.get('username');
+    this.student_id = this.auth.auth.currentUser.uid;
+    this.loadTransactions();
+  }
+
+  //Function to load the transactions of the user and display them
+  loadTransactions(){
+    this.db.list('/users/'+this.student_id+'/transactions/').valueChanges().subscribe((d) => {
+      this.transaction_list = d;
+      console.log(this.transaction_list);
+    });
   }
 
   //To remove the swipe menu in this page
