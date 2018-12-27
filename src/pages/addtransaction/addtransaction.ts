28672f6a0eab4ded8efb24935aaaf2d06fcce5e3
Christian Montero Martínez <christianmontero08@gmail.com>
2018-12-27 00:28:46 -0800

    Update the setTransaction function

--- src/pages/addtransaction/addtransaction.ts
+++ src/pages/addtransaction/addtransaction.ts
@@ -22,15 +22,17 @@ export class AddtransactionPage {
   @ViewChild('amount') amount;
   errorMessage: string;
   payment_mehtod: string = "Select payment method";
+  category: string = "";
   payment_id;
-  transaction_type;
+  category_id;
+  transaction_type = 'income';
   items;
   payment_list;
   category_list;
   user_id; 
 
   //Litheral object constructor
-  transaction = {type: 0, concept : '', amount : 0, paymentId : 0, datetime: '', id: 0};
+  transaction = {type: 0, concept : '', amount : 0, category: '', paymentId : 0, datetime: '', id: 0};
   transaction_types: string[] = ["INCOME", "CASH WITHDRAWAL", "EXPENSE"];
   status_messages: string[] = ["Successfully registered transaction","The concept field is required", "The amount field is required", "There was a problem with the server"];
 
@@ -79,29 +81,54 @@ export class AddtransactionPage {
   addTransaction(){
     //This is needed in all transactions
     this.setTransaction(this.transaction);  //Set the transaction
-    var paymnet_balance = this.payment_list[this.transaction.paymentId - 1].balance;  //The payment id is the index + 1 of the payment method in the payment_list
+    var payment_balance = this.payment_list[this.transaction.paymentId - 1].balance;  //The payment id is the index + 1 of the payment method in the payment_list
     var cash_balance = this.payment_list[0].balance;  //The payment id is the index + 1 of the payment method in the payment_list
-    var transaction_amount = this.transaction.amount; 
-    this.db.object('/users/'+this.user_id+'/transactions/'+this.transaction.id+'/').set(this.transaction);  //Store the transaction in the database
-    this.db.list('/users/').update('/'+this.user_id+'/',{num_transactions:this.transaction.id + 1});  //Update the num_transactions of the user
-    //If the transaction type is income
-    if(this.transaction_type == 0){
-      this.db.list('/users/').update('/'+this.user_id+'/payment/'+this.transaction.paymentId+'/',{balance: parseInt(paymnet_balance) + (transaction_amount | 0)});  //Update the balance of the used payment method
-    }else if(this.transaction_type == 1){
-      this.db.list('/users/').update('/'+this.user_id+'/payment/'+this.transaction.paymentId+'/',{balance: (paymnet_balance - transaction_amount)});  //Reduce the balance of the used payment method
-      this.db.list('/users/').update('/'+this.user_id+'/payment/1/',{balance: parseInt(cash_balance) + (transaction_amount | 0)});  //Increase the cash
-    }else{
-      this.db.list('/users/').update('/'+this.user_id+'/payment/'+this.transaction.paymentId+'/',{balance: (paymnet_balance - transaction_amount)});  //Reduce the balance of the used payment method
-    }
+    var transaction_amount = this.transaction.amount;
+    console.log('the transaction is: ', this.transaction);
+    // this.db.object('/users/'+this.user_id+'/transactions/'+this.transaction.id+'/').set(this.transaction);  //Store the transaction in the database
+    // this.db.list('/users/').update('/'+this.user_id+'/',{num_transactions:this.transaction.id + 1});  //Update the num_transactions of the user
+    // if(this.transaction_type == 'income'){
+    //   this.setIncome(payment_balance, transaction_amount);
+    // }else if(this.transaction_type == 'expenses'){
+    //   this.setExpense(payment_balance, transaction_amount, cash_balance);
+    // }else{
+    //   this.setTransfer(payment_balance, transaction_amount);      
+    // }
+  }
+
+  setIncome(payment_balance, transaction_amount){
+    this.db.list('/users/').update('/'+this.user_id+'/payment/'+this.transaction.paymentId+'/',{balance: parseInt(payment_balance) + (transaction_amount | 0)});  //Update the balance of the used payment method
+  }
+
+  setExpense(payment_balance, transaction_amount, cash_balance){
+    this.db.list('/users/').update('/'+this.user_id+'/payment/'+this.transaction.paymentId+'/',{balance: (payment_balance - transaction_amount)});  //Reduce the balance of the used payment method
+    this.db.list('/users/').update('/'+this.user_id+'/payment/1/',{balance: parseInt(cash_balance) + (transaction_amount | 0)});  //Increase the cash
+  }
+
+  setTransfer(payment_balance, transaction_amount){
+    this.db.list('/users/').update('/'+this.user_id+'/payment/'+this.transaction.paymentId+'/',{balance: (payment_balance - transaction_amount)});  //Reduce the balance of the used payment method
   }
 
   //Setting the texfield values to the properties of our object
   setTransaction(transaction){
-    transaction.type = this.transaction_type;
-    transaction.id = this.items.num_transactions;
-    transaction.amount = this.amount.value;
-    transaction.paymentId = this.payment_list[this.payment_id - 1].id;
-    transaction.datetime = new Date().toLocaleString();
+    if(this.transaction_type == 'income'){
+      transaction.amount = this.amount.value;
+      transaction.datetime = new Date().toLocaleString();
+      transaction.id = this.items.num_transactions;
+      transaction.type = this.transaction_type;
+    }else if(this.transaction_type == 'expenses'){
+      transaction.amount = this.amount.value;
+      transaction.datetime = new Date().toLocaleString();
+      transaction.id = this.items.num_transactions;
+      transaction.type = this.transaction_type;
+      transaction.paymentId = this.payment_list[this.payment_id - 1].id;
+      transaction.category = this.category_list[this.category_id - 1].id;
+    }
+    
+    
+    
+    
+    
     if(this.transaction_type == 2){
       transaction.concept = this.concept.value;
     }
@@ -113,6 +140,14 @@ export class AddtransactionPage {
     this.payment_id = id;
     this.payment_mehtod = this.payment_list[id];
   }
+
+  getCategoryId(id){
+    this.category_id = id;
+    this.category = this.category_list[id];
+    console.log('The selected category is: ', this.category);
+    
+  }
+
   //Function to get the transaction type (the value will change everytime a different value is selected)
   getTransactionType(transaction_type){
     this.transaction_type = transaction_type;
